4fcb3803bdced29a2e6486e78f50f91765b7f643
Ade Syahlan Prayoga <imalanpryoga@gmail.com>
2022-11-27 19:24:37 +0700

    Fix docker compose env vars

--- wallet-service/src/main/java/com/github/alanpryoga/springewalletapp/walletservice/WalletServiceApplication.java
+++ wallet-service/src/main/java/com/github/alanpryoga/springewalletapp/walletservice/WalletServiceApplication.java
@@ -1,5 +1,6 @@
 package com.github.alanpryoga.springewalletapp.walletservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,13 +12,22 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @SpringBootApplication
 public class WalletServiceApplication {
 
+	@Value("${spring.redis.host}")
+	private String REDIS_HOST;
+
+	@Value("${spring.redis.port}")
+	private Integer REDIS_PORT;
+
 	public static void main(String[] args) {
 		SpringApplication.run(WalletServiceApplication.class, args);
 	}
 
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
-		return new JedisConnectionFactory();
+		JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
+		jedisConFactory.setHostName(REDIS_HOST);
+		jedisConFactory.setPort(REDIS_PORT);
+		return jedisConFactory;
 	}
 
 	@Bean
